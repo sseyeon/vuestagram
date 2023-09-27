517cb4dfc9f0b183d244c8a20ac7da03099b7e89
sseyeon <seyeon9809@gmail.com>
2023-09-28 02:51:28 +0900

    Container, Post 페이지 작업

---
+++ src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Post :게시물="게시물[i]" v-for="(a,i) in 게시물" :key="i"/>
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue';
+export default {
+    // name: 'Container',
+    components: {
+        Post : Post,
+    },
+    props : {
+        게시물 : Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
